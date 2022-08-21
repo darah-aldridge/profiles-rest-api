ef2e3a6cf03222e5ea862d718c67860bd9fd22d4
Darah <darah.aldridge@gmail.com>
2022-08-21 12:44:23 -0500

    Added user classes but need to troubleshoot LookupError: No installed app with label 'admin'.

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.authtoken',
+    'rest_framwork.authtoken',
     'profiles_api',
 ]
 
